eaa95df27706e805fe531e855b94f772f464a79e
Radosław Skupnik <r.skupnik@gmail.com>
2025-09-26 13:19:32 +0200

    Dummy code change to test caching

--- acolyte/api.py
+++ acolyte/api.py
@@ -31,8 +31,6 @@ async def scrape(payload: ScrapeIn = Body(...)) -> ScrapeAccepted:
     job = jobs.create_job(payload.script_id, webhook)
     args = payload.args or {}
 
-    print("Scrape received")
-
     import asyncio
     asyncio.create_task(jobs.run_job(job, runner, args))
     return ScrapeAccepted(accepted=True, job_id=job.job_id)
